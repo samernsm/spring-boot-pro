44258d38d0b8b64900cb9af72e2353edfcb8c916
samernsm <samer_nsm@yahoo.com>
2021-12-27 12:00:34 +0200

    test3

--- book-store/src/main/java/com/global/bookstore/controller/AutherController.java
+++ book-store/src/main/java/com/global/bookstore/controller/AutherController.java
@@ -1,10 +1,52 @@
 package com.global.bookstore.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.global.bookstore.entity.Auther;
+import com.global.bookstore.service.AutherService;
 
 @RestController
 @RequestMapping("/auther")
 public class AutherController {
+	
+	private AutherService autherService;
+
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		return ResponseEntity.ok(autherService.findById(id));
+	}
+     
+	@GetMapping("")
+	public ResponseEntity<?> findAll(){
+		return   ResponseEntity.ok(autherService.findAll());
+	}
+	@PostMapping("")
+	public ResponseEntity<?> insert(Auther entity) {
+				 
+		return ResponseEntity.ok(autherService.insert(entity));
+	}
+
+	@PutMapping("")
+	public ResponseEntity<?> update(Auther entity) {
+				
+			
+		return  ResponseEntity.ok(autherService.update(entity));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
+		
+		autherService.delete(id);
+		return ResponseEntity.ok(null);
+		
+	}
 
 }
